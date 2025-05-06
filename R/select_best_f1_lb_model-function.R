ce96f04ab799980e89f30722c8766dc17141e6ca
herdiantrisufriyana <herdiantrisufriyana@gmail.com>
2025-05-06 12:57:41 +0800

    Allow zero well-calibrated models.

--- R/select_best_f1_lb_model-function.R
+++ R/select_best_f1_lb_model-function.R
@@ -1,12 +1,16 @@
 select_best_f1_lb_model <- function(discrimination_obj_list){
-  discrimination_obj_list |>
-    imap(
-      ~ .x$validation |>
-        mutate(model = .y)
-    ) |>
-    reduce(rbind) |>
-    filter(metric == "f1") |>
-    filter(lb == max(lb)) |>
-    slice(1) |>
-    pull(model)
+  if(length(discrimination_obj_list) == 0){
+    character()
+  }else{
+    discrimination_obj_list |>
+      imap(
+        ~ .x$validation |>
+          mutate(model = .y)
+      ) |>
+      reduce(rbind) |>
+      filter(metric == "f1") |>
+      filter(lb == max(lb)) |>
+      slice(1) |>
+      pull(model)
+  }
 }
