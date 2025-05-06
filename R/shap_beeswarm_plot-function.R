026c4297a03fe1c53a7ed22a787e79e9410f66d4
herdiantrisufriyana <herdiantrisufriyana@gmail.com>
2025-05-06 12:58:19 +0800

    Correct SHAP beeswarm coloring and positioning.

--- R/shap_beeswarm_plot-function.R
+++ R/shap_beeswarm_plot-function.R
@@ -1,14 +1,38 @@
 shap_beeswarm_plot <-
-  function(data, seed, scaled = TRUE, transparency = 1, barheight = 4){
+  function(
+    data, samp_size = 1, shap_iqr_width = 1.5, seed, transparency = 0.5
+    , expected_value_name = "expected_value"
+  ){
     
-    if(scaled){
-      data <-
-        data |>
-        group_by(feature) |>
-        mutate(feature_value = scale(feature_value)) |>
-        ungroup() 
-    }
     set.seed(seed)
+    data <-
+      data |>
+      group_by(feature, feature_value) |>
+      filter(seq %in% sample(unique(seq), ceiling(samp_size * n()), FALSE)) |>
+      ungroup()
+    
+    data <-
+      data |>
+      filter(feature != expected_value_name) |>
+      group_by(feature) |>
+      mutate(
+        feature_value = rank(feature_value)
+        , feature_value =
+          (feature_value - min(feature_value))
+        / (max(feature_value) - min(feature_value))
+      ) |>
+      ungroup() |>
+      mutate(
+        shap_iqr = quantile(shap_value, 0.75) - quantile(shap_value, 0.25)
+        , shap_lb =
+          quantile(shap_value, 0.25)
+          - ifelse(shap_iqr == 0, shap_iqr_width, shap_iqr_width * shap_iqr)
+        , shap_ub =
+          quantile(shap_value, 0.75)
+          + ifelse(shap_iqr == 0, shap_iqr_width, shap_iqr_width * shap_iqr)
+      ) |>
+      filter(shap_value >= shap_lb & shap_value <= shap_ub) |>
+      select(-shap_iqr, -shap_lb, -shap_ub)
     
     data <-
       data |>
@@ -25,97 +49,26 @@ shap_beeswarm_plot <-
           0.5 * direction + 0.5 * magnitude
       ) |>
       mutate(feature = reorder(feature, magnitude)) |>
-      arrange(seq, feature) |>
-      mutate(shap_value_bin = round(shap_value * 100, 0) / 100) |>
+      arrange(seq, feature)|>
+      group_by(feature) |>
+      mutate(
+        shap_value_bin = rank(shap_value)
+        , shap_value_bin =
+          (shap_value_bin - min(shap_value_bin))
+        / (max(shap_value_bin) - min(shap_value_bin))
+        , shap_value_bin =
+          round(shap_value_bin * 100, 0) / 100
+      ) |>
+      ungroup() |>
       group_by(feature, shap_value_bin) |>
       mutate(freq = n()) |>
       ungroup()|>
       mutate(
-        jitter_width = freq / max(freq) * 0.3
+        jitter_width = 0.3 - (abs(shap_value_bin - 0.5) / 0.5 * 0.3)
         , feature_num = as.numeric(feature)
-        , jitter_feature = feature_num + runif(n(), -jitter_width, jitter_width)
+        , jitter_feature = feature_num + runif(freq, -jitter_width, jitter_width)
       )
     
     data |>
-      ggplot(aes(x = jitter_feature, color = feature_value)) +
-      geom_hline(yintercept = 0, color = "grey", linewidth = 1) +
-      geom_point(
-        aes(y = shap_value)
-        , position = "identity"
-        , size = 1.5
-        , alpha = transparency
-      ) +
-      geom_errorbar(
-        data =
-          data |>
-          mutate(
-            feature_value_cat =
-              ifelse(
-                feature_value >= mean(data$feature_value)
-                , max(data$feature_value)
-                , min(data$feature_value)
-              )
-          ) |>
-          group_by(feature_num, feature_value_cat) |>
-          summarize(
-            avg = mean(shap_value)
-            , std = sd(shap_value)
-            , .groups = "drop"
-          ) |>
-          mutate(
-            lb = avg - qnorm(0.975) * std
-            , ub = avg + qnorm(0.975) * std
-          )
-        , aes(
-            x =
-              ifelse(feature_value_cat == max(feature_value_cat), feature_num, NA)
-            , ymin = lb
-            , ymax = ub
-            , color = feature_value_cat
-          )
-        , width = 0.25, na.rm = TRUE
-      ) +
-      coord_flip() +
-      scale_x_continuous(
-        breaks = unique(select(data, feature, feature_num))$feature_num
-        ,labels =
-          paste0(
-            unique(select(data, feature, feature_num))$feature
-            , ' - '
-            , max(data$feature_num)
-              - unique(select(data, feature,feature_num))$feature_num
-              + 1
-          )
-      ) +
-      scale_color_gradient(
-        "Feature value"
-        ,low = "#008AFB"
-        ,high = "#FF0053"
-        ,breaks = c(min(data$feature_value), max(data$feature_value))
-        ,labels = c("Low", "High")
-      ) +
-      theme_minimal() +
-      xlab("") +
-      ylab("SHAP value (impact on model output)") +
-      theme(
-        panel.grid.major = element_blank()
-        , panel.grid.minor = element_blank()
-        , axis.ticks.x = element_line()
-        , axis.line.x = element_line()
-        , legend.position = "right"
-        , legend.title = element_text(angle = 90, hjust = 0.5)
-      ) +
-      guides(
-        color = 
-          guide_colorbar(
-            barwidth = 0.2
-            , barheight = barheight
-            , title.position = "right"
-            , title.hjust = 0.5
-            , label.hjust = 0.5
-            , ticks = FALSE
-            , draw.ulim = TRUE
-            , draw.llim = TRUE
-          )
-      )
+      shap_beeswarm_plotter(transparency)
   }
